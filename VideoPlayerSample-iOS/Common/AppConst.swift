69943513b9bad77c17b24d45e7eaac0af4c33873
isaoeka <isaoeka@gmail.com>
2018-10-14 16:33:57 +0900

    :sparkles: Add const file

---
+++ VideoPlayerSample-iOS/Common/AppConst.swift
@@ -0,0 +1,13 @@
+//
+//  AppConst.swift
+//  VideoPlayerSample-iOS
+//
+//  Created by isaoeka on 2018/10/14.
+//  Copyright © 2018 isaoeka. All rights reserved.
+//
+
+struct AppConst {
+    enum ApiUrl: String {
+        case videoList = ""
+    }
+}
